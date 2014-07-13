b33521d776a29cf23cc63ec7939f7372c041a541
Anders D. Johnson <adjohnson916@gmail.com>
2014-07-13 08:30:14 -0500

    update example with 2, including no smooth

--- example/js/index.js
+++ example/js/index.js
@@ -1,10 +1,24 @@
 
-var $z = $('.mg-zoom');
+$(function () {
 
-var z = new Zoom({
-  element: $z,
-  src: {
-    lg: 'img/color-md.jpg',
-    md: 'img/color-md.jpg'
-  }
+  var $zoom1 = $('.my-zoom-1');
+  var zoom1 = new Zoom({
+    element: $zoom1,
+    src: {
+      lg: 'img/color-md.jpg',
+      md: 'img/color-md.jpg'
+    }
+  });
+
+  var $zoom2 = $('.my-zoom-2');
+  var zoom2 = new Zoom({
+    element: $zoom2,
+    smooth: false,
+    src: {
+      lg: 'img/color-md.jpg',
+      md: 'img/color-md.jpg'
+    }
+  });
+
+
 });
